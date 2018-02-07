e74817fa13b3b445f8e5d84941998fe3c57306b4
Tanay Shah <tanayshah007@gmail.com>
2018-02-06 20:21:07 -0600

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -49,53 +49,6 @@ def rescale(df, type='minmax'):
     rescaled_data.columns = df.columns
     return rescaled_data
 
-def handle_missing_data(df, missing_features = ['BXOWgPgL', 'McFBIGsm', 'BRzuVmyf', 'OSmfjCbE'],
-                        remove_features = ['umkFMfvA','FGWqGkmD','IrxBnWxE','dnlnKrAg','aAufyreG']):
-    if remove_features!=[]:
-        df.drop(remove_features, axis=1, inplace=True)
-    # df = standardize(df)
-    df = df.sample(frac=1).reset_index(drop=True)
-    complete_rows = df.dropna()
-    missing_rows = df[df.isnull().any(axis=1)]
-    data = complete_rows.drop(missing_features, axis=1)
-    #standardise df for test data
-    id = data['id']
-    id = id.reset_index(drop=True)
-    data.drop('id', axis=1, inplace=True)
-    data = standardize(data)
-    data = pd.DataFrame(data)
-    data['id'] = id
-    data.columns = complete_rows.drop(missing_features, axis=1).columns
-    data['id'] = id
-    validation_index = data.shape[0] - round(0.2*data.shape[0])
-    training_data = data[:validation_index]
-    validation_data = data[validation_index:]
-    # temp_data = missing_rows.drop(missing_features, axis=1)
-    for feature in missing_features:
-        labels = complete_rows[feature]
-        labels_training = labels[:validation_index]
-        labels_validation = labels[validation_index:]
-        temp_data = missing_rows[missing_rows[feature].isnull()]
-        test_data = temp_data.drop(missing_features, axis=1)
-
-        regressor = Lasso(alpha=1)
-        regressor.fit(training_data.drop('id', axis=1), labels_training)
-        predictions_val = regressor.predict(validation_data.drop('id',axis=1))
-
-        val_loss = mean_absolute_error(labels_validation, predictions_val)
-        print("loss for",feature,":", val_loss)
-
-        # predictions = regressor.predict(test_data.drop('id',axis=1))
-
-def find_missing_values(df):
-    def num_missing(x):
-        return sum(x.isnull())
-
-    pd.set_option("display.max_rows", 999) #uncomment to display all columns
-    missing_values_per_column = df.apply(num_missing, axis=0)
-    print (missing_values_per_column)
-    print('Number of missing values:', sum(missing_values_per_column))
-
 def missing_data(df, missing_features = ['BXOWgPgL', 'McFBIGsm', 'BRzuVmyf', 'OSmfjCbE'],
                 remove_features = ['umkFMfvA','FGWqGkmD','IrxBnWxE','dnlnKrAg','aAufyreG']):
 
